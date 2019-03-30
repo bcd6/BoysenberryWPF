62d0c1e03a5640f1a8bab8c820e7286a7f39814f
Lucciola <bcd6@live.cn>
2019-03-30 23:21:13 +1300

    Add logo

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace BoysenberryWPF
+namespace Boysenberry
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
